f03449805a2c98e3d103796d9c21fa6f9e585c0a
Gustavo <gustavo@auriga>
2013-08-27 10:35:29 -0300

    login insert, person random, date for admin insert

--- meals/models.py
+++ meals/models.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Meal(models.Model):                                                    
     date = models.DateField()
-    
+    ordered = models.BooleanField(default=False)
+    washer = models.ForeignKey('people.Person', null=True, blank=True)
+    ticket = models.PositiveSmallIntegerField(null=True, blank=True)
+        
     def __unicode__(self):
         return unicode(self.date)
 
@@ -10,7 +16,6 @@ class PersonMeal(models.Model):
     person = models.ForeignKey('people.Person')
     meal = models.ForeignKey('Meal')
     wash = models.BooleanField(default=False)
-    
+
     class Meta:
-        unique_together = ('person', 'meal', 'wash')
-    
+        unique_together = (('person', 'meal'),)
--- people/models.py
+++ people/models.py
@@ -6,8 +6,20 @@ class Person(models.Model):
     def count_wash(self):
         #countwash = PersonMeal.objects.filter(wash=True, person=self).count()
         #nesse caso seria preciso importar PersonMeal
-        countwash = Person.personmeal_set.filter(wash=True).count()
-        return countwash
+        return self.personmeal_set.filter(wash=True).count()
+
+    def count_eat(self):
+        # PersonMeal.object.filter(person=self).count()
+        return self.personmeal_set.count()
+
+    def get_average(self):
+        eat = self.count_eat()
+        if not eat:
+            return 0.0
+        return round(float(self.count_wash())/self.count_eat(), 3)
+
+   #def maximum(self):
+   #    return self.objects.all().get(Max('get_average()'))
 
     def __unicode__(self):
         return self.name
