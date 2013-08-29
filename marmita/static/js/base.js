30732355f4b91fda9ba206a4f37b0154fb004404
Gustavo <gustavo@tracy.com.br>
2013-08-29 09:24:06 -0300

    static insert

---
+++ marmita/static/js/base.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#datepicker").datepicker({
+        showOn: "button",
+        buttonImage: "../assets/jquery-ui-1.10.3.custom/images/calendar.gif",
+        buttonImageOnly: true
+    });
+});
